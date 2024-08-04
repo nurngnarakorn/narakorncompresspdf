21e82b0c69a558b1008e76ab84ff8e3877105681
nurngnarakorn <nurng.narakorn@gmail.com>
2024-08-04 22:52:08 +0700

    add gtm
    add gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Script from "next/script";
 
-const GTM_ID = "GTM-5Z5MDZKB";
+const GTM_ID = "GTM-K6KMJ6Q3";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -33,20 +33,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        id="gtmtagid"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
-          `,
-        }}
-      />
-      <Component {...pageProps} />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K6KMJ6Q3"
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        ></iframe>
+      </noscript>
       <SpeedInsights />
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
+const GTM_ID = "GTM-K6KMJ6Q3";
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* <Script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2877713589134666"
-              crossOrigin="anonymous"
-              id="adsense-banner"
-            > */}
+          <Script
+            id="gtmtagid"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+            }}
+          />
           <Script
             id="adsense-banner"
             async
